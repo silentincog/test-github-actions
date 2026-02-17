9dbde000d1ce4f1f61bfb7908f56ae01e3749db6
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-02-17 15:22:32 +0545

    Updated the deploy.yaml worflow to setup adding the server to the runner's known_hosts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3333
 
 app.get('/', (req, res) => {
-    res.send('Hello Incog here v1 before github actions !!\n')
+    res.send('Hello Incog here v1 after implementing github actions !!\n')
 })
 
 
